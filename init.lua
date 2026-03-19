c008c9cf53aab92e33fcff9803e0bf2f6bcd21de
Fed <mfedore1@gmail.com>
2026-03-18 22:26:35 -0400

    sane spacing

--- init.lua
+++ init.lua
@@ -134,7 +134,7 @@ vim.api.nvim_create_autocmd('FileType', {
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
+  vim.opt.clipboard = 'unnamedplus'
 end)
 
 -- Enable break indent
@@ -184,6 +184,11 @@ vim.o.scrolloff = 10
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+
+-- set tab size to 4 :help tabstop
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
